b89afb0645f2ccbdc14476288fc53911a72c8207
wcrane <1191814251@qq.com>
2024-10-04 19:29:02 +0800

    publish version 1.0.34

--- publish.config.js
+++ publish.config.js
@@ -1,17 +1,26 @@
-import { execSync } from 'child_process'
-export default {
-    root: ".", // 项目根目录
-    syncGit: true, // 是否同步到git
-    // 发布版本号自动升级等级
+
+module.exports = {
+    // 发布目录
+    root: ".",
+    // 是否同步git
+    syncGit: true,
+    // 是否同步git tag
+    syncGitTag: true,
+    // 升级版本号的等级
     versionLevel: 'patch', // major | minor | patch
-    customPublish: false, // 是否自定义发布
+    // 自定义发布
+    customPublish: false,
     // 发布前执行
     before(config) {
         // console.log(config)
-        // execSync('npm publish')
     },
     // 发布后执行
     after(config) {
         // console.log(config)
-    }
+    },
+    // git tag 格式
+    gitTagFormat: (version) => {
+        return `v${version}`
+    },
 }
+
