2c7752274880dffd8261315ab04e5eaece3eb462
wcrane <1191814251@qq.com>
2024-10-05 15:39:52 +0800

    publish version 1.1.14

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 
 export default defineConfig({
-    entry: ['src/index.ts', "src/workder.ts"],
+    entry: ['src/index.ts', "src/workder.ts", "src/imageWorker.worker.js"],
     splitting: false,
     sourcemap: false,
     clean: true,
